e51d52d46adcce5f12816c18da091278e5e77136
Zarmeenq <zarmeenq@nycstudents.net>
2026-01-06 19:05:54 +0000

    erhre

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function Rasmalai(){
+    document.getElementById("highlight").innerHTML="Rasmalai";
+}
+
+function Rabri(){
+    document.getElementById("highlight").innerHTML="Rabri";
+}
+
+function Poori(){
+    document.getElementById("highlight").innerHTML="Poori";
+}
+
+function Biryani(){
+    document.getElementById("highlight").innerHTML="Biryani";
+}
+
+function AlooGosht(){
+    document.getElementById("highlight").innerHTML="AlooGosht";
+}
