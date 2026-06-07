7058cf20c26ac98a385d42b82a423e6fc862edbd
Zarmeenq <zarmeenq@nycstudents.net>
2026-06-07 05:46:32 +0000

    xdx

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -22,4 +22,16 @@ async function init(){
               </div>` ;   
   }
   output.innerHTML = build;
+
+}
+
+let data, mapObj;
+
+function displayMap(){
+  //Retrieve the latitude & longitude from the user via text inputs and pass it to the showMap() function to generate the map and display it.
+  let lat = get("lat").value;
+  let lon = get("lon").value;
+
+  showMap(lat,lon);
+
 }
